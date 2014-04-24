3515dbfb3a64f6eb762e3bdbb73d65339984374e
Kevin Sullivan <sideslapd@gmail.com>
2014-04-24 16:42:39 -0400

    Standard backup commit
    Continued fleshing out the program, implemented incoming threading and
    receipt of messages, removed extraneous comments.

--- main.c
+++ main.c
@@ -17,64 +17,91 @@
 
 #define BUFFER_SIZE 1024
 
+// Struct to maintain socket information
+typedef struct {
+	int newSock;
+} sockStruct;
+
 // Handle all communication between the client and server
 void* handleComm(void* arg)
 {
-	int received;
+	int received, sent;
+	char *response = NULL;
+	char buffer[BUFFER_SIZE];
+	long int threadID = (long int)pthread_self();
+	sockStruct *data = arg;
 
 	// Endless loop for servet/client communication
 	while (1)
 	{
-		received = recv(newsock, buffer, BUFFER_SIZE - 1, 0);
+		// Block until something is received from the client
+		received = recv(data->newSock, buffer, BUFFER_SIZE - 1, 0);
+
+		// Check for receiving error
+		if (received < 0)
+		{
+			printf("Error: Unable to parse received data\n");
+			continue;
+		}
+
+		// See if the client closed its socket
+		else if (received == 0)
+		{
+			printf("[thead %ld] Client closed its socket....terminating\n");
+			break;
+		}
+
+		// Receive the data if all is well
+		else
+		{
+			// Print receipt message
+			printf("[thead %ld] Rcvd: %s", threadID, buffer);
+
+			// Check for ADD command
+			if (strncmp(buffer, "ADD", 3) == 0)
+			{
+				printf("IT WORKS\n");
+			}
+
+			// Check for DELETE command
+			else if (strncmp(buffer, "DELETE", 6) == 0)
+			{
+
+			}
+
+			// Check for the READ command
+			else if (strncmp(buffer, "READ", 4) == 0)
+			{
+
+			}
+
+			// Otherwise, this is an ERROR
+			else
+			{
+				sent = send(newSock, response, strlen(response), 0);
+			}
+
+
+
+
+
+
+
+
+			// sent = send(newSock, response, strlen(response), 0);
+
+
+
+
+			// n = send( newsock, msg, strlen( msg ), 0 );
+   //    if ( n < strlen( msg ) ) {
+   //      perror( "Write()" );
+   //    }
+		}
 	}
 
-
-
-
-
-
-
-     while ( 1 )
-     {
-      /* can also use read() and write() */
-      n = recv( newsock, buffer, BUFFER_SIZE - 1, 0 ); // BLOCK
-      if ( n < 0 )
-      {
-        perror( "recv()" );
-        break;
-      }
-      else if ( n == 0 )
-      {
-        printf( "Client closed its socket\n" );
-        break;
-      }
-      else {
-        buffer[n] = '\0';
-        printf( "Received message from %s: %s\n",
-                inet_ntoa( (struct in_addr)client.sin_addr ),
-                buffer );
-      }
-      n = send( newsock, msg, strlen( msg ), 0 );
-      if ( n < strlen( msg ) ) {
-        perror( "Write()" );
-      }
-     }
-
-      close( newsock );
-      exit( 0 );
-      /* child process becomes a zombie... */
-
-
-
-
-
-
-
-
-
-	printf("Thread %ld\n", (long int)pthread_self());
-	wait(10000);
-
+	close(data->newSock);
+	
 	return NULL;
 }
 
@@ -85,7 +112,6 @@ int main(int argc, char* argv[])
 	int sock, newSock, portNumber, len, rc;
 	unsigned int fromLen;
 	pthread_t tid;
-
 
 	// Make sure one argument is included
 	if (argc != 2)
@@ -154,270 +180,13 @@ int main(int argc, char* argv[])
 		// Notify when a client connection comes in
 		printf("Recieved incoming connection from %s\n", inet_ntoa(client.sin_addr));
 
+		// Create a struct to pass information to the thread
+		sockStruct *data = (sockStruct*)malloc(sizeof *data);
+		data->newSock = newSock;
+
 		// Spawn thead for client communication
-		rc = pthread_create(&tid, NULL, handleComm, 0);
+		rc = pthread_create(&tid, NULL, handleComm, data);
 	}
-
-
-
-
-//   while ( 1 )
-//   {
-//     printf( "Blocked on accept()\n" );
-//     newsock = accept( sock, (struct sockaddr *)&client, &fromlen );
-//     /* newsock is another file (socket) descriptor */
-
-
-//     printf( "Accepted client connection\n" );
-
-//     printf( "Client is %s\n", inet_ntoa( client.sin_addr ) );
-
-
-//     /* handle socket in child process */
-//     pid = fork();
-
-//     if ( pid == 0 )   /* CHILD */
-//     {
-//      while ( 1 )
-//      {
-//       /* can also use read() and write() */
-//       n = recv( newsock, buffer, BUFFER_SIZE - 1, 0 ); // BLOCK
-//       if ( n < 0 )
-//       {
-//         perror( "recv()" );
-//         break;
-//       }
-//       else if ( n == 0 )
-//       {
-//         printf( "Client closed its socket\n" );
-//         break;
-//       }
-//       else {
-//         buffer[n] = '\0';
-//         printf( "Received message from %s: %s\n",
-//                 inet_ntoa( (struct in_addr)client.sin_addr ),
-//                 buffer );
-//       }
-//       n = send( newsock, msg, strlen( msg ), 0 );
-//       if ( n < strlen( msg ) ) {
-//         perror( "Write()" );
-//       }
-//      }
-
-//       close( newsock );
-//       exit( 0 );
-//       /* child process becomes a zombie... */
-//     }
-
-//     /* parent simply closes the socket */
-//     close( newsock );
-//   }
-
-// close( newsock );
-// exit( 0 );
-// /* child process becomes a zombie... */
-// }
-
-// /* parent simply closes the socket */
-// close( newsock );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// long maxPath;
-	// int i;
-	// int index = 0;
-	// char* backupPaths[99999];
-	// unsigned long long totalSize = 0;
-	// int totalFiles = 0;
-	// int totalDirectories = 0;
-	// bool restoreFlag = false;
-	// bool unknownFlag = false;
-
-	// // Determine the max pathname allowed on this PC
-	// if ((maxPath = pathconf("/", _PC_PATH_MAX)) == -1)
-	// 	maxPath = 4096;
-
-	// // Increase space for string delimeter
-	// maxPath++;
-
-	// char cwd[maxPath];
-
-	// if (getcwd(cwd, sizeof(cwd)) == NULL)
-	// 	printf("Error obtaining working directory!");
-		
-
-	// for (i = 0; i < 99999; i++)
-	// 	backupPaths[i] = malloc((maxPath + 1) * sizeof(char));
-
-	// // Check for init error
-	// if (backupPaths[i-1] == NULL)
-	// 	return 1;
-
-	// // Parse command line arguments
-	// for (i = 1; i < argc; i++) 
-	// {
-	// 	if (strcmp(argv[i], "-r") == 0) 
-	// 		restoreFlag = true; 
-	// 	else  
-	// 		unknownFlag = true;
-	// }
-
-	// // Show a warning if unknown command line arguments were provided
-	// if (unknownFlag) 
-	// {
-	// 	printf("An unknown command line argument has been provided and was\n");
-	// 	printf("ignored. Only -r for restoring a backup is supported\n");
-	// }		
-
-	// // Make the backup directory if it does not exist
-	// mkdir(".mybackup", S_IRWXU);
-
-	// if (restoreFlag)
-	// {
-	// 	// Get all paths to restore
-	// 	int final = getPaths("./.mybackup", maxPath, backupPaths, index);
-	// 	backupPaths[final] = NULL;
-
-	// 	// Spawn threads for each restore function
-	// 	int i, rc;
-	// 	totalSize = 0;
-	// 	totalFiles = 0;
-	// 	totalDirectories = 0;
-	// 	pthread_t tid[final - 1];
-
-	// 	for ( i = 0 ; i < final - 1 ; i++ )
-	// 	{
-	// 		if (backupPaths[i] == NULL)
-	// 			break;
-
-	// 		struct stat s;
-	// 		stat(backupPaths[i], &s);
-
-	// 		backupStruct *args = (backupStruct*)malloc(sizeof *args);
-	// 		args->pathname = malloc(maxPath * sizeof(char));
-	// 		strcpy(args->pathname, backupPaths[i]);
-	// 		args->maxPath = maxPath;
-
-	// 		if(S_ISDIR(s.st_mode)) 
-	// 		{
- //    			// Create the directory if needed
- //       			rc = pthread_create(&tid[i], NULL, restoreBackupDir, args);
- //       			totalDirectories++;
-	// 			pthread_join(tid[i], NULL);
-	// 		} else {
-	// 		    if(S_ISREG(s.st_mode)) {
-	// 		    	// Skip the executable name
-	// 		    	if (strcmp(basename(args->pathname), "mybackup") == 0)
-	// 		    		continue;
-
-	// 		        // Directory exists
-	// 				FILE* source = fopen(args->pathname, "r");
-
-	// 				// Get the filesize
-	// 				fseek(source, 0L, SEEK_END);
-	// 				int size = ftell(source);
-	// 				totalSize += size;
-	// 				totalFiles++;
-	// 				args->size = size;
-
-	// 				fclose(source);
-
-	// 				// Spawn the restoration thread
-	// 				rc = pthread_create(&tid[i], NULL, restore, args);
-	// 		    } 
-	// 		}
-
-	// 		if (rc != 0) 
-	// 			perror("Could not create thread");
-	// 	}
-
-	// 	for ( i = 0 ; i < final - 1 ; i++ )
-	// 		pthread_join(tid[i], NULL);
-
-	// 	printf("Successfully restored %d files (%llu bytes) in %d subdirectories\n", totalFiles, totalSize, totalDirectories);
-	// }
-	// else
-	// {
-	// 	// Get all paths to back up
-	// 	int final = getPaths(".", maxPath, backupPaths, index);
-	// 	backupPaths[final] = NULL;
-
-	// 	// Spawn threads for each backup function
-	// 	int i, rc;
-	// 	totalSize = 0;
-	// 	totalFiles = 0;
-	// 	totalDirectories = 0;
-	// 	pthread_t tid[final - 1];
-
-	// 	for ( i = 0 ; i < final - 1 ; i++ )
-	// 	{
-	// 		if (backupPaths[i] == NULL)
-	// 			break;
-
-	// 		struct stat s;
-	// 		stat(backupPaths[i], &s);
-
-	// 		backupStruct *args = (backupStruct*)malloc(sizeof *args);
-	// 		args->pathname = malloc(maxPath * sizeof(char));
-	// 		strcpy(args->pathname, backupPaths[i]);
-	// 		args->maxPath = maxPath;
-
-	// 		if(S_ISDIR(s.st_mode)) 
-	// 		{
- //    			// Create the directory if needed
- //       			rc = pthread_create(&tid[i], NULL, makeBackupDir, args);
- //       			totalDirectories++;
- //       			pthread_join(tid[i], NULL);
-	// 		} else {
-	// 		    if(S_ISREG(s.st_mode)) {
-	// 				// Skip the executable name
-	// 		    	if (strcmp(basename(args->pathname), "mybackup") == 0)
-	// 		    		continue;
-
-	// 		        // Directory exists
-	// 				FILE* source = fopen(args->pathname, "r");
-
-	// 				// Get the filesize
-	// 				fseek(source, 0L, SEEK_END);
-	// 				int size = ftell(source);
-	// 				totalFiles++;
-	// 				args->size = size;
-
-	// 				fclose(source);
-
-	// 				// Spawn the backup thread
-	// 				rc = pthread_create(&tid[i], NULL, backup, args);
-	// 		    } 
-	// 		}
-
-	// 		if (rc != 0) 
-	// 			perror("Could not create thread");\
-	// 	}
-
-	// 	// Join the threads and count the total bytes
-	// 	for ( i = 0 ; i < final - 1 ; i++ )
-	// 	{
-	// 		void* status;
-
-	// 		pthread_join(tid[i], &status);
-
-	// 		if ((intptr_t)status > 0)
-	// 			totalSize += (intptr_t)status;
-	// 	}
-
-	// 	printf("Successfully backed up %d files (%llu bytes) in %d subdirectories\n", totalFiles, totalSize, totalDirectories);
-	// }
 
 	return 0;
 }
